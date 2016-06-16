459536d99a0f83f0d5272f6cb13cf4b324f2c654
allan <epicallan.al@gmail.com>
2016-06-16 15:35:06 +0300

    deploy file changes

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 [![build status](https://img.shields.io/travis/epicallan/docker-continuous-deployment/master.svg?style=flat-square)](https://travis-ci.org/epicallan/docker-continuous-deployment)
 
-- This is a test project for Continuous deployment and testing using docker
+- This is a project for Continuous deployment using docker
   and github.
-- There is an app on the server which is listening for new commits via github hooks
+- There is an [app on the server](https://github.com/epicallan/github-deploy-server) which is listening for new commits via github hooks
 - The app runs a [bash script](https://github.com/epicallan/docker-continuous-deployment/blob/master/deploy.sh) that rebuilds the docker app container
 - The script deletes the current application directory and fetches the latest repo from github.
 
--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,13 @@ git clone git@github.com:epicallan/docker-continuous-deployment.git ~/app.git
 
 isFirstBuild=`docker images | grep -w web`
 
-if [[ !$isFirstBuild ]]; then
-  echo 'first build so we run composer up for all the images'
-  docker build -t web . # builds app image
-  docker-compose up # links up the various compose image containers
-else
+docker build -t web . # re-builds the web app image
+
+if [ ! -z "$isFirstBuild" ]; then\
   echo 'updating the web app image'
   docker-compose rm -f web # remove container associated with web image
-  docker build -t web . # re-builds the web app image
   docker-compose up -d --no-deps web # creates a new container for the web image without restarting the other containers
+else
+  echo 'first build so we run composer up for all the images'
+  docker-compose up # links up the various compose image containers
 fi
